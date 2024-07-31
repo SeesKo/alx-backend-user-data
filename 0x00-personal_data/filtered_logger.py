a31c177f6050f83d7ebee045f7a832f3e14a4bbd
SeesKo <seeskome@seeskosee.com>
2024-07-31 08:15:42 -0700

    fix credentials

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -80,7 +80,7 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
         'user': os.getenv('PERSONAL_DATA_DB_USERNAME', 'root'),
         'password': os.getenv('PERSONAL_DATA_DB_PASSWORD', ''),
         'host': os.getenv('PERSONAL_DATA_DB_HOST', 'localhost'),
-        'database': os.getenv('PERSONAL_DATA_DB_NAME')
+        'database': os.getenv('PERSONAL_DATA_DB_NAME', '')
     }
 
     # Establish the database connection

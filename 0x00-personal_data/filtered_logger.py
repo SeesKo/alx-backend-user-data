c7fd80128c226739a8ac7c7f7bf67bcea28a69f8
SeesKo <seeskome@seeskosee.com>
2024-07-31 09:02:26 -0700

    fix credentials

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -102,18 +102,23 @@ def main() -> None:
     """
     Main function to fetch and display user data with filtered PII.
     """
-    logger = get_logger()
-    db = get_db()
-    cursor = db.cursor(dictionary=True)
-
-    cursor.execute("SELECT * FROM users;")
-    for row in cursor:
-        # Construct log message
-        message = "; ".join(f"{key}={value}" for key, value in row.items())
-        logger.info(message)
-
-    cursor.close()
-    db.close()
+    fields = "name,email,phone,ssn,password,ip,last_login,user_agent"
+    columns = fields.split(',')
+    query = "SELECT {} FROM users;".format(fields)
+    info_logger = get_logger()
+    connection = get_db()
+    with connection.cursor() as cursor:
+        cursor.execute(query)
+        rows = cursor.fetchall()
+        for row in rows:
+            record = map(
+                lambda x: '{}={}'.format(x[0], x[1]),
+                zip(columns, row),
+            )
+            msg = '{};'.format('; '.join(list(record)))
+            args = ("user_data", logging.INFO, None, None, msg, None, None)
+            log_record = logging.LogRecord(*args)
+            info_logger.handle(log_record)
 
 
 if __name__ == "__main__":

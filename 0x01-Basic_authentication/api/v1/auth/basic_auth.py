c50819f78c0c3131c3bba7b41c4fa6c570a5e56c
SeesKo <seeskome@seeskosee.com>
2024-08-05 08:54:23 -0700

    basic authentication module

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Basic Authentication module for the API
+"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """ Basic Authentication class that inherits from Auth """
+    pass

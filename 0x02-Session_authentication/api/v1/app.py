52e9651cd24bf74da8345b84d6dc487e270c7afb
SeesKo <seeskome@seeskosee.com>
2024-08-07 11:07:20 -0700

    SessionExpAuth instantiation

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python3
 """
-This module sets up the Flask application and handles API routing,
-authentication, and error handling.
+Route module for the API
 """
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
 from api.v1.auth.session_auth import SessionAuth
+from api.v1.auth.session_exp_auth import SessionExpAuth
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import CORS
 from os import getenv
+
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -22,6 +23,8 @@ if auth_type == "basic_auth":
     auth = BasicAuth()
 elif auth_type == "session_auth":
     auth = SessionAuth()
+elif auth_type == "session_exp_auth":
+    auth = SessionExpAuth()
 else:
     auth = Auth()
 

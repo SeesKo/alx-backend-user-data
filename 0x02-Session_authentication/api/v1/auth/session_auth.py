b2fc05a3110cbafe78b8204c829c333776f76aeb
SeesKo <seeskome@seeskosee.com>
2024-08-07 18:35:26 -0700

    destroy user session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -62,3 +62,16 @@ class SessionAuth(Auth):
 
         # Retrieve the User instance from the database
         return User.get(user_id)
+
+    def destroy_session(self, request=None) -> bool:
+        """Destroys the user session"""
+        if request is None:
+            return False
+        session_id = self.session_cookie(request)
+        if session_id is None:
+            return False
+        user_id = self.user_id_for_session_id(session_id)
+        if user_id is None:
+            return False
+        del self.user_id_by_session_id[session_id]
+        return True

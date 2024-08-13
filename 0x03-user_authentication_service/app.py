d14ba7a11880743b832d8e3e5bc59aab4766f634
SeesKo <seeskome@seeskosee.com>
2024-08-13 21:41:21 +0000

    handle profile view

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -69,5 +69,18 @@ def logout():
     return redirect('/')
 
 
+@app.route('/profile', methods=['GET'])
+def profile():
+    """Handle GET /profile request"""
+    # Get session_id from cookies
+    session_id = request.cookies.get('session_id')
+
+    # Find the user based on session_id
+    user = AUTH.get_user_from_session_id(session_id)
+
+    AUTH.destroy_session(user.id)
+    return jsonify({"email": user.email})
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")

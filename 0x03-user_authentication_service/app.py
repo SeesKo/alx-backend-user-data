b715c44c9968885aaba70ab14a2f371f92c69e61
SeesKo <seeskome@seeskosee.com>
2024-08-13 18:36:08 +0000

    log in a user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,7 +2,7 @@
 """
 Flask app with user registration
 """
-from flask import Flask, abort, jsonify, request
+from flask import Flask, abort, jsonify, request, make_response
 from auth import Auth
 
 
@@ -31,3 +31,23 @@ def users() -> str:
         return jsonify({"email": user.email, "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route("/sessions", methods=["POST"])
+def login() -> str:
+    """Log in a user and create a session."""
+    email: Optional[str] = request.form.get("email")
+    password: Optional[str] = request.form.get("password")
+
+    if not email or not password:
+        abort(400, description="Missing email or password")
+
+    if AUTH.valid_login(email, password):
+        session_id: str = AUTH.create_session(email)
+        response: Response = make_response(
+            jsonify({"email": email, "message": "logged in"})
+        )
+        response.set_cookie("session_id", session_id)
+        return response
+    else:
+        abort(401, description="Invalid credentials")

ea0256f9f279fa831650765f98d27b376ef50764
SeesKo <seeskome@seeskosee.com>
2024-08-13 16:51:36 +0000

    fix sqlite mode

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -16,7 +16,7 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -37,7 +37,7 @@ class DB:
             new_user = User(email=email, hashed_password=hashed_password)
             self._session.add(new_user)
             self._session.commit()
-        except Exception:
+            return new_user
+        except SQLAlchemyError as e:
             self._session.rollback()
             return None
-        return new_user

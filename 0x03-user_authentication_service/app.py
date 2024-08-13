85671df745f55430f930d8837834ae9c112a0ac8
SeesKo <seeskome@seeskosee.com>
2024-08-13 11:22:43 +0000

    initialize flask application

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
+"""A simple Flask app with user authentication features.
 """
-Basic Flask app with user registration
-"""
-from flask import Flask, abort, jsonify, request
+from flask import Flask, jsonify, request
 from auth import Auth
 
 
@@ -10,21 +9,25 @@ app = Flask(__name__)
 AUTH = Auth()
 
 
-@app.route("/", methods=["GET"])
-def welcome() -> str:
-    """GET route that returns a welcome message."""
+@app.route("/", methods=["GET"], strict_slashes=False)
+def index() -> str:
+    """GET /
+    Return:
+        - The home page's payload.
+    """
     return jsonify({"message": "Bienvenue"})
 
 
-@app.route("/users", methods=["POST"])
-def register_user() -> str:
-    """POST /users route for user registration."""
-    email = request.form.get('email')
-    password = request.form.get('password')
-
+@app.route("/users", methods=["POST"], strict_slashes=False)
+def users() -> str:
+    """POST /users
+    Return:
+        - The account creation payload.
+    """
+    email, password = request.form.get("email"), request.form.get("password")
     try:
-        user = AUTH.register_user(email, password)
-        return jsonify({"email": user.email, "message": "user created"})
+        AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
 

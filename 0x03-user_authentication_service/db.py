17a06def8259d64b2e446824231e5285ce38e24f
SeesKo <seeskome@seeskosee.com>
2024-08-13 16:56:40 +0000

    fix db module

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -10,11 +10,11 @@ from user import Base, User
 
 
 class DB:
-    """DB class
+    """DB class.
     """
 
     def __init__(self) -> None:
-        """Initialize a new DB instance
+        """Initialize a new DB instance.
         """
         self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
@@ -23,7 +23,7 @@ class DB:
 
     @property
     def _session(self) -> Session:
-        """Memoized session object
+        """Memoized session object.
         """
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
@@ -31,13 +31,13 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """Adds a user to the database and returns the User object
+        """Adds a new user to the database.
         """
         try:
             new_user = User(email=email, hashed_password=hashed_password)
             self._session.add(new_user)
             self._session.commit()
-            return new_user
-        except SQLAlchemyError as e:
+        except Exception:
             self._session.rollback()
-            return None
+            new_user = None
+        return new_user

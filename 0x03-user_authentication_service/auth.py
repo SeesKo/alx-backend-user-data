9648016d2952730e67dc3367bb11f22ba7b94cfa
SeesKo <seeskome@seeskosee.com>
2024-08-13 21:53:00 +0000

    generate password reset token

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -87,3 +87,17 @@ class Auth:
             self._db.update_user(user_id, session_id=None)
         except NoResultFound:
             pass
+
+    def get_reset_password_token(self, email: str) -> str:
+        """Generates a reset password token for the user and returns it
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            raise ValueError
+
+        reset_token = self._generate_uuid()
+        user.reset_token = reset_token
+        self._db._session.commit()
+
+        return reset_token

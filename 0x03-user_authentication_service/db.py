aa2700d9d5f35d1665204f27891928233a0b310d
SeesKo <seeskome@seeskosee.com>
2024-08-13 16:42:25 +0000

    add new user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -34,7 +34,7 @@ class DB:
             new_user = User(email=email, hashed_password=hashed_password)
             self._session.add(new_user)
             self._session.commit()
-            return new_user
-        except SQLAlchemyError as e:
+        except Exception:
             self._session.rollback()
             return None
+        return new_user

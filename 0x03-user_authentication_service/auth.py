cd2a3c94280db77aa1f4bc3ce54fd40fac5c2f50
SeesKo <seeskome@seeskosee.com>
2024-08-13 12:04:11 +0000

    authentication module

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -8,26 +8,28 @@ from user import User
 import bcrypt
 
 
-def _hash_password(self, password: str) -> bytes:
-    """Hashes a password using bcrypt
-    and returns the salted hash.
-    """
-    return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
-
-
 class Auth:
     """Auth class to interact with the authentication database.
     """
 
-    def __init__(self):
+    def __init__(self) -> None:
+        """Initialize the Auth class with a database instance.
+        """
         self._db = DB()
+
+    def _hash_password(self, password: str) -> bytes:
+        """Hashes a password using bcrypt
+        and returns the salted hash.
+        """
+        return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
 
     def register_user(self, email: str, password: str) -> User:
         """Registers a new user with an email and password.
         """
         try:
             self._db.find_user_by(email=email)
+            raise ValueError(f"User {email} already exists")
         except NoResultFound:
-            user = self._db.add_user(email, _hash_password(password))
+            hashed_password = self._hash_password(password)
+            user = self._db.add_user(email, hashed_password)
             return user
-        raise ValueError(f"User {email} already exists")
